ef07e6e6cdb3d101f7d238c38a816b6570919dd3
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2024-11-28 17:37:08 +0330

    Update main.py

--- classes.py
+++ classes.py
@@ -1,8 +1,9 @@
 class Employee:
-    def __init__(self, name, birthyear, salary):
+    def __init__(self, name, birthyear, salary, company):
         self.name = name
         self.birthyear = birthyear
         self.salary = salary
+        self.company = company
 
     def set_user_pass(self, username, password):
         self.username = username
@@ -29,49 +30,70 @@ class Employee:
 
 
 class Programmer(Employee):
-    def get_all_projs(self, company):
-        return company.get_all_projs()
+    def get_all_projs(self):
+        return self.company.get_all_projs()
     
-    def add_proj(self, company, proj):
-        company.add_proj(proj)
+    def add_proj(self, proj):
+        self.company.add_proj(proj)
     
-    def remove_proj(self, company, proj):
-        company.remove_proj(proj)
-
-
-
+    def remove_proj(self, proj):
+        self.company.remove_proj(proj)
 
 
 
 class Tester(Employee):
-    def accept_proj(self, company, proj):
-        for p in company.projs:
+    def accept_proj(self, proj):
+        for p in self.company.projs:
             if p[0] == proj:
                 p[1] = True
 
-    def decline_proj(self, company, proj):
-        for p in company.projs:
+    def decline_proj(self, proj):
+        for p in self.company.projs:
             if p[0] == proj:
                 p[1] = False
 
 
 
 
+class Super_Man(Programmer, Tester):
+    def add_emp(self, name, birth, salary, role):
+        if role == "Employee":
+            new_emp = Employee(name, birth, salary, self.company)
+        
+        if role == "Programmer":
+            new_emp = Programmer(name, birth, salary, self.company)
+        
+        if role == "Tester":
+            new_emp = Tester(name, birth, salary, self.company)
+        
+        if role == "Manager":
+            new_emp = Super_Man(name, birth, salary, self.company)
+        
+        self.company.add_emp(new_emp, role)
 
+    def remove_emp(self, emp, role):
+        self.company.remove_emp(emp, role)
 
-class Manager(Programmer, Tester):
-    pass
+    def get_all_emps(self):
+        return self.company.get_all_emps()
 
 
 class Company:
     def __init__(self):
         self.emps = {
-            "Employees":[],
-            "Programmers":[],
-            "Testers":[],
-            "Managers":[]
+            "Employee":[],
+            "Programmer":[],
+            "Tester":[],
+            "Manager":[]
         }
         self.projs = []
+
+    def search_user(self, username, password):
+        for role, emps in self.emps.items():
+            for emp in emps:
+                if emp.login(username, password):
+                    return (role, emp)
+        raise ValueError("Username or Password was wrong!")
 
     def add_emp(self, emp, role):
         self.emps[role].append(emp)
---
+++ mian.py
@@ -0,0 +1,50 @@
+from classes import *
+
+def emp_menu(emp: Employee):
+    message = f"""
+Hello {emp.name}! Welcome back.
+What do you wanna do today?
+1. See your info
+2. Change your info
+""" 
+    print(message)
+    userIn = input("Type your choice: ")
+
+    if userIn == '1':
+        print(emp.get_info())
+    elif userIn == '2':
+        # Complete this at home!
+        pass
+    
+    else:
+        print("Wrong input, Try again!")
+
+def man_menu(man: Super_Man):
+    print(f"Hello {man.name}! welcome to managers' menu.")
+
+def main():
+    main_company = Company()
+    main_manager = Super_Man("Erfan", "2002", "1500", main_company)
+    main_manager.username = "Erfan"
+    main_manager.password = "1234"
+    main_company.add_emp(main_manager, "Manager")
+
+    print("Welcome to Erfan's Portal. Please type you username and password.")
+
+    while True:
+        username = input("username: ")
+        password = input("password: ")
+
+        try:
+            main_company.search_user(username, password)
+        except ValueError as v:
+            print(v)
+            continue
+        else:
+            man_menu(main_manager)
+            break
+
+
+
+
+main()
